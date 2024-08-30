d5fcf69fed05fb7f53a1d3fb9af3a125f5643e3a
Arthur63 <arthur63@163.com>
2024-08-30 19:54:51 +0800

    refactor code

--- silicon/main.c
+++ silicon/main.c
@@ -1,17 +1,42 @@
 #include <curl/curl.h>
 #include <jansson.h>
 
-#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+
+enum
+{
+  TYPE_MODELS,
+  TYPE_INFO,
+};
+
+typedef struct result_t result_t;
+typedef struct pack_t pack_t;
+
+struct result_t
+{
+  bool ok;
+  char key[55];
+  char name[12];
+  char balance[16];
+};
+
+struct pack_t
+{
+  int type;
+  result_t *res;
+
+  char *data;
+  size_t size;
+
+  struct curl_slist *hdrs;
+};
 
 static void init (void);
 static void cleanup (void);
 static void work (const char *path);
 
-static FILE *result;
-static FILE *models;
 static CURLM *multi;
 static bool got_models;
 
@@ -28,46 +53,14 @@ init (void)
 {
   curl_global_init (CURL_GLOBAL_ALL);
   multi = curl_multi_init ();
-  assert (multi);
-
-  remove ("./result");
-  remove ("./models");
-
-  result = fopen ("./result", "a");
-  models = fopen ("./models", "w");
-  assert (result && models);
-
-  fputs ("key, name, balance\n", result);
 }
 
 static void
 cleanup (void)
 {
-  fclose (models);
-  fclose (result);
   curl_multi_cleanup (&multi);
   curl_global_cleanup ();
 }
-
-enum
-{
-  TYPE_MODELS,
-  TYPE_INFO,
-};
-
-typedef struct pack_t pack_t;
-
-struct pack_t
-{
-  int type;  /* query type */
-  char *key; /* from strdup */
-
-  char *data;  /* from cb  */
-  size_t size; /* from cb  */
-
-  CURL *hnd;
-  struct curl_slist *hdrs;
-};
 
 static void
 get_models_hnd (pack_t *pack)
@@ -76,17 +69,19 @@ get_models_hnd (pack_t *pack)
 
   if (json)
     {
-      fprintf (models, "%s\n", pack->key);
+      FILE *models = fopen ("./models", "w");
       json_t *arr = json_object_get (json, "data");
+
       for (size_t n = json_array_size (arr), i = 0; i < n; i++)
         {
-          json_t *item = json_array_get (arr, i);
-          json_t *id = json_object_get (item, "id");
+          json_t *id = json_object_get (json_array_get (arr, i), "id");
           fputs (json_string_value (id), models);
           fputc ('\n', models);
         }
-      got_models = true;
+
       json_decref (json);
+      got_models = true;
+      fclose (models);
     }
 }
 
@@ -111,7 +106,7 @@ get_models_cb (char *data, size_t size, size_t nmemb, void *ptr)
 }
 
 static void
-get_models (const char *key)
+get_models (result_t *res)
 {
   CURL *hnd = curl_easy_init ();
   pack_t *pack = malloc (sizeof (pack_t));
@@ -122,13 +117,12 @@ get_models (const char *key)
   curl_easy_setopt (hnd, CURLOPT_CUSTOMREQUEST, "GET");
 
   static char hdr_key[80];
-  sprintf (hdr_key, "authorization: Bearer %s", key);
+  sprintf (hdr_key, "authorization: Bearer %s", res->key);
 
   struct curl_slist *hdrs = NULL;
   hdrs = curl_slist_append (hdrs, hdr_key);
   hdrs = curl_slist_append (hdrs, "accept: application/json");
 
-  char *dup = strdup (key);
   curl_easy_setopt (hnd, CURLOPT_PRIVATE, pack);
   curl_easy_setopt (hnd, CURLOPT_WRITEDATA, pack);
   curl_easy_setopt (hnd, CURLOPT_HTTPHEADER, hdrs);
@@ -136,32 +130,32 @@ get_models (const char *key)
 
   curl_multi_add_handle (multi, hnd);
 
-  pack->key = dup;
-  pack->type = TYPE_MODELS;
+  pack->res = res;
 
   pack->size = 0;
   pack->data = NULL;
 
-  pack->hnd = hnd;
   pack->hdrs = hdrs;
+  pack->type = TYPE_MODELS;
 }
 
 static void
 get_info_hnd (pack_t *pack)
 {
+  result_t *res = pack->res;
   json_t *json = json_loadb (pack->data, pack->size, 0, NULL);
 
   if (json)
     {
       json_t *obj = json_object_get (json, "data");
-      const char *name = json_string_value (json_object_get (obj, "name"));
-      const char *balance
-          = json_string_value (json_object_get (obj, "balance"));
-      fprintf (result, "%s, %s, %s\n", pack->key, name, balance);
+      json_t *name = json_object_get (obj, "name");
+      json_t *balance = json_object_get (obj, "balance");
+
+      strcpy (res->balance, json_string_value (balance));
+      strcpy (res->name, json_string_value (name));
       json_decref (json);
+      res->ok = true;
     }
-  else
-    fprintf (result, "%s, , \n", pack->key);
 }
 
 static size_t
@@ -182,7 +176,7 @@ get_info_cb (char *data, size_t size, size_t nmemb, void *ptr)
 }
 
 static void
-get_info (const char *key)
+get_info (result_t *res)
 {
   CURL *hnd = curl_easy_init ();
   pack_t *pack = malloc (sizeof (pack_t));
@@ -193,13 +187,12 @@ get_info (const char *key)
   curl_easy_setopt (hnd, CURLOPT_URL, URL_INFO);
 
   static char hdr_key[80];
-  sprintf (hdr_key, "authorization: Bearer %s", key);
+  sprintf (hdr_key, "authorization: Bearer %s", res->key);
 
   struct curl_slist *hdrs = NULL;
   hdrs = curl_slist_append (hdrs, hdr_key);
   hdrs = curl_slist_append (hdrs, "accept: application/json");
 
-  char *dup = strdup (key);
   curl_easy_setopt (hnd, CURLOPT_PRIVATE, pack);
   curl_easy_setopt (hnd, CURLOPT_WRITEDATA, pack);
   curl_easy_setopt (hnd, CURLOPT_HTTPHEADER, hdrs);
@@ -207,37 +200,42 @@ get_info (const char *key)
 
   curl_multi_add_handle (multi, hnd);
 
-  pack->key = dup;
+  pack->res = res;
   pack->type = TYPE_INFO;
 
   pack->size = 0;
   pack->data = NULL;
 
-  pack->hnd = hnd;
   pack->hdrs = hdrs;
 }
 
 static void
 work (const char *path)
 {
+  int num = 0;
+  result_t *results[1024];
   FILE *file = fopen (path, "r");
-  assert (file);
 
   for (char key[64]; fgets (key, sizeof (key), file);)
     {
       if (key[0] == '\n')
         continue;
 
-      key[strlen (key) - 1] = '\0';
+      size_t len = strlen (key);
+      key[len - 1] = '\0';
 
-      get_models (key);
-      get_info (key);
+      result_t *res = malloc (sizeof (result_t));
+      memcpy (res->key, key, len + 1);
+      res->ok = false;
+
+      results[num++] = res;
+      get_models (res);
+      get_info (res);
     }
 
   for (int n = 1, r; n;)
     {
-      CURLMcode code = curl_multi_perform (multi, &n);
-
+      curl_multi_perform (multi, &n);
       curl_multi_poll (multi, NULL, 0, 1000, NULL);
 
       for (struct CURLMsg *m; (m = curl_multi_info_read (multi, &r));)
@@ -246,24 +244,42 @@ work (const char *path)
             long code;
             pack_t *pack;
             CURL *hnd = m->easy_handle;
+
             curl_easy_getinfo (hnd, CURLINFO_PRIVATE, &pack);
             curl_easy_getinfo (hnd, CURLINFO_RESPONSE_CODE, &code);
 
             if (code != 200)
-              continue;
+              goto clean;
 
             if (pack->type == TYPE_MODELS && !got_models)
               get_models_hnd (pack);
             else if (pack->type == TYPE_INFO)
               get_info_hnd (pack);
 
+          clean:
             curl_slist_free_all (pack->hdrs);
             free (pack->data);
-            free (pack->key);
             free (pack);
 
-            curl_multi_remove_handle (multi, m->easy_handle);
+            curl_multi_remove_handle (multi, hnd);
             curl_easy_cleanup (hnd);
           }
+    }
+
+  FILE *result = fopen ("./result", "w");
+  fputs ("key, name, balance\n", result);
+
+  for (int i = 0; i < num; i++)
+    {
+      result_t *res = results[i];
+      bool ok = res->ok;
+
+      char *key = res->key;
+      char *name = ok ? res->name : "";
+      char *balance = ok ? res->balance : "";
+
+      fprintf (result, "%s, %s, %s\n", key, name, balance);
+
+      free (res);
     }
 }
