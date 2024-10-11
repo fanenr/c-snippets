7c39b5ebc8a7b911af190fe2e6a96bbfec44e93c
Arthur63 <arthur63@163.com>
2024-10-11 18:28:05 +0800

    update

--- event/04.c
+++ event/04.c
@@ -2,12 +2,6 @@
 #include <event2/event.h>
 #include <event2/http.h>
 
-#include <sys/socket.h>
-#include <unistd.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-
 void
 handle_request (struct evhttp_request *req, void *arg)
 {
@@ -25,7 +19,7 @@ main (void)
   base = event_base_new ();
   http = evhttp_new (base);
 
-  evhttp_bind_socket (http, "127.0.0.1", 3354);
+  evhttp_bind_socket (http, "0.0.0.0", 3354);
   evhttp_set_gencb (http, handle_request, NULL);
 
   event_base_dispatch (base);
