49d6d4c3a4d562ab32d2bc39df7df0ecd1acea14
Arthur63 <arthur63@163.com>
2024-10-14 09:51:42 +0800

    update

--- uring/cat.c
+++ uring/cat.c
@@ -6,6 +6,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define QUEUE_SIZE 64
+#define BLK_SIZE 4096
+
 #define error(code, fmt, ...)                                                 \
   do                                                                          \
     {                                                                         \
@@ -17,9 +20,6 @@
     }                                                                         \
   while (0)
 
-#define QUEUE_SIZE 16
-#define BLK_SIZE 4096
-
 #define submit(ring)                                                          \
   do                                                                          \
     {                                                                         \
@@ -28,6 +28,25 @@
         error (1, "%s", strerror (-ret));                                     \
     }                                                                         \
   while (0)
+
+#define check_cqe(cqe)                                                        \
+  do                                                                          \
+    {                                                                         \
+      union io_data data = { cqe->user_data };                                \
+      if (cqe->res != data.data.len)                                          \
+        error (1, "failed to read");                                          \
+    }                                                                         \
+  while (0)
+
+union io_data
+{
+  __u64 u64;
+  struct
+  {
+    int op;
+    unsigned len;
+  } data;
+};
 
 size_t
 sizeof_fd (int fd)
@@ -38,17 +57,42 @@ sizeof_fd (int fd)
   return stat.st_size;
 }
 
-void
-prep_read (struct io_uring *ring, int fd, void *buff, unsigned len, off_t off)
+struct io_uring_sqe *
+get_sqe (struct io_uring *ring)
 {
+  int ret;
+  struct io_uring_cqe *cqe;
   struct io_uring_sqe *sqe;
+
+get:
   if ((sqe = io_uring_get_sqe (ring)))
-    return io_uring_prep_read (sqe, fd, buff, len, off);
+    return sqe;
 
   submit (ring);
 
-  sqe = io_uring_get_sqe (ring);
+  if ((ret = io_uring_wait_cqe (ring, &cqe)))
+    error (1, "%s", strerror (-ret));
+
+  check_cqe (cqe);
+  io_uring_cqe_seen (ring, cqe);
+
+  for (; !io_uring_peek_cqe (ring, &cqe); io_uring_cqe_seen (ring, cqe))
+    check_cqe (cqe);
+
+  goto get;
+}
+
+void
+prep_read (struct io_uring *ring, int fd, void *buff, unsigned len, off_t off)
+{
+  union io_data data;
+  struct io_uring_sqe *sqe;
+
+  sqe = get_sqe (ring);
   io_uring_prep_read (sqe, fd, buff, len, off);
+
+  data.data.len = len;
+  sqe->user_data = data.u64;
 }
 
 int
@@ -81,24 +125,29 @@ main (int argc, char **argv)
     if (!(blks[i] = aligned_alloc (BLK_SIZE, BLK_SIZE)))
       error (1, "%s", strerror (errno));
 
-  for (size_t i = 0; i < nblks; i++)
-    prep_read (&ring, fd, blks[i], BLK_SIZE, i * BLK_SIZE);
+  for (size_t i = 0, copy = size; i < nblks; i++)
+    {
+      size_t len = copy > BLK_SIZE ? BLK_SIZE : copy;
+      prep_read (&ring, fd, blks[i], len, i * BLK_SIZE);
+      copy -= len;
+    }
 
   submit (&ring);
 
-  for (size_t i = 0; i < nblks; i++)
+  for (struct io_uring_cqe *cqe;
+       io_uring_sq_ready (&ring) || io_uring_cq_ready (&ring);
+       io_uring_cqe_seen (&ring, cqe))
     {
-      struct io_uring_cqe *cqe;
       if ((ret = io_uring_wait_cqe (&ring, &cqe)))
         error (1, "%s", strerror (-ret));
-      io_uring_cqe_seen (&ring, cqe);
+      check_cqe (cqe);
     }
 
   for (size_t i = 0; i < nblks; i++)
     {
-      size_t buff_size = size > BLK_SIZE ? BLK_SIZE : size;
-      if (fwrite (blks[i], 1, buff_size, stdout) != buff_size)
+      size_t len = size > BLK_SIZE ? BLK_SIZE : size;
+      if (fwrite (blks[i], 1, len, stdout) != len)
         error (1, "%s", strerror (errno));
-      size -= buff_size;
+      size -= len;
     }
 }
